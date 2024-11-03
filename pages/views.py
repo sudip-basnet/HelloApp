5e487d6c3dca5cb823adff756c6658114afd29eb
sudip-basnet <sudipkazibasnet@gmail.com>
2024-11-03 12:09:02 +0545

    add home and about page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse("Hello World!")
-    
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+# def homePageView(request):
+#     return HttpResponse("Hello World!"
+class AboutPageView(TemplateView):
+    template_name = "about.html"
